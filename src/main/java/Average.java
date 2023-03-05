891525302a31e633d4665e95abbb2012dacf49c2
Ronny Barahona <ronnybarahona10@gmail.com>
2023-03-05 16:40:25 +0000

    hmm

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,12 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        double avg = (double) sum / nums.length;
+        return avg;
     }
+    
 }
